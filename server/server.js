01d1a7612251e3d1f064f8965eb7a1521f166485
blyncnov <bholuwatife00@gmail.com>
2022-10-14 16:08:54 +0100

    server updated

--- server/server.js
+++ server/server.js
@@ -1,51 +1,13 @@
-const http = require("http");
-const express = require("express");
-
-const dotenv = require("dotenv");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
-const cors = require("cors");
 
-const app = express();
-const server = http.createServer(app);
-
-// .env config
-dotenv.config();
-
-// CORS_MIDDLEWARE
-app.use(cors());
-
-// io connection
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  // options
 });
 
-// PORT connections
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.status(200).send("hello socket io");
-});
-
-app.get("/test", (req, res) => {
-  res.status(200).json({
-    status: "sucess",
-    message: "hello socket io",
-  });
-});
-
-// Whenever someone connects this gets executed
 io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  // Whenever someone disconnects this piece of code executed
-  socket.on("disconnect", function () {
-    console.log("A user disconnected");
-  });
+  // ...
 });
 
-app.listen(PORT, () => {
-  console.log(`App Running ON PORT ${PORT}`);
-});
+httpServer.listen(3000);
