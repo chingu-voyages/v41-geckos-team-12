c889b4b24899c62682ade6b17d242406bc6dbfb6
fmtabbara <fmtabbara@hotmail.co.uk>
2022-10-15 16:19:02 +0100

    fix typo

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-const container = document.getElementById('app')!;
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App  />);
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+const container = document.getElementById('app')!
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(<App />)
