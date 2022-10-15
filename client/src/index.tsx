1a1c11d97e88f0334c2c36f6ee3ea9d648b49e1a
fmtabbara <fmtabbara@hotmail.co.uk>
2022-10-15 16:29:01 +0100

    update lit settings

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-const container = document.getElementById('app')!
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
-root.render(<App />)
+const container = document.getElementById('app')
+
+if (container) {
+  const root = createRoot(container) // createRoot(container!) if you use TypeScript
+  root.render(<App />)
+}
