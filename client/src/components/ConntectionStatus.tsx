77c2e4eba305acecc1f7d0aa128c6c5195bc23ea
fmtabbara <fmtabbara@hotmail.co.uk>
2022-10-22 00:03:25 +0100

    set up styling

--- client/src/components/ConntectionStatus.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { useAppContext } from '../context'
-
-export const ConnectionStatus = () => {
-  const { socket } = useAppContext()
-
-  return <h1>Connected: {socket?.connected ? '✅' : '❌'}</h1>
-}
