8f2f25bb3ae3069a6811ec84ac16aaeb1b6715f2
fmtabbara <fmtabbara@hotmail.co.uk>
2022-10-24 15:51:03 +0100

    merge develop

--- client/src/layouts/index.tsx
+++ client/src/layouts/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Logo } from '../components/Logo'
-import { Button } from '../components/Button'
 import './styles.scss'
 
-export const AppLayout = ({ children }: { children: React.ReactNode }) => (
+export const AppLayout = ({
+  children,
+  SideNav,
+}: {
+  children: React.ReactNode
+  SideNav: React.ReactElement
+}) => (
   <div className="app-layout">
     <div className="app-side-bar">
       <Logo />
       {/* nav goes here */}
-      <h1 className="title-name">Chatterbox</h1>
-      <Button variant="outlined">Leave chat</Button>
+      {SideNav}
     </div>
     <div className="app-content">{children}</div>
   </div>
