227fd701652a01bdffdbc52b08e88b64785b2e80
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-10-24 11:01:48 +0200

    added navbar items

--- client/src/layouts/index.tsx
+++ client/src/layouts/index.tsx
@@ -7,6 +7,8 @@ export const AppLayout = ({ children }: { children: React.ReactNode }) => (
     <div className="app-side-bar">
       <Logo />
       {/* nav goes here */}
+      <h1 className="title-name">Chatterbox</h1>
+      <button className="leave-chat-btn">Leave chat</button>
     </div>
     <div className="app-content">{children}</div>
   </div>
