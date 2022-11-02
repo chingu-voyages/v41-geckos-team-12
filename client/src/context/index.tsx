3adc750885485abf79e1867ab454ce10fa4521ce
fmtabbara <fmtabbara@hotmail.co.uk>
2022-11-02 22:57:22 +0000

    update path

--- client/src/context/index.tsx
+++ client/src/context/index.tsx
@@ -24,7 +24,7 @@ const AppContext = createContext({} as AppContext)
 export const useAppContext = () => useContext(AppContext)
 
 const establishConnection = () =>
-  io('https://chatterbox.onrender.com/socket.io', { autoConnect: false })
+  io('https://chatterbox.onrender.com', { autoConnect: false })
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [socket] = useState<Socket>(establishConnection())
