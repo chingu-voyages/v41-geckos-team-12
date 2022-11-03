fa6cd7cb36bb9866a9f25d3591ef23b77525312e
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-11-03 14:39:49 +0100

    fixed hour to sent messages

--- client/src/components/Message/index.tsx
+++ client/src/components/Message/index.tsx
@@ -7,9 +7,15 @@ export const Message = ({ message }: { message: MessageType }) => (
   <div className="msg-container">
     <div className={`message p-m ${message.self ? 'self' : ''}`}>
       {message.message}
+<<<<<<< HEAD
       <div className="msg-date">
         <MessageDate date={message.date} />
       </div>
+=======
+    </div>
+    <div className={`msg-date ${message.self ? 'self' : ''}`}>
+      <MessageDate date={message.date} />
+>>>>>>> e7173d5 (fixed hour to sent messages)
     </div>
   </div>
 )
